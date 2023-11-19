931523829c0240da2d4f1f08f147bfba6646312d
minjyun3011 <grand3canyon10@gmail.com>
2023-11-19 17:08:24 +0900

    修正完了

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
-
 class CalcController extends Controller
 {
     public function result($num1, $operator, $num2)
@@ -11,34 +9,30 @@ class CalcController extends Controller
         switch ($operator) {
             case 'addition':
                 $calc = $num1 + $num2;
-                return view('calcs.addition', ['calc' => $calc]);
+                $calc_type = "足し";
                 break;
             case 'subtraction':
                 $calc = $num1 - $num2;
-                return view('calcs.subtraction', ['calc' => $calc]);
-
+                $calc_type = "引き";
                 break;
             case 'multiplication':
                 $calc = $num1 * $num2;
-                return view('calcs.multiplication', ['calc' => $calc]);
-
+                $calc_type = "かけ";
                 break;
             case 'division':
                 if ($num2 != 0) {
                     $calc = $num1 / $num2;
-                    return view('calcs.division', ['calc' => $calc]);
+                    $calc_type = "割り";
                 } else {
                     // ゼロでの割り算は避けるか、エラー処理を追加するなど
                     // ここではエラーメッセージを表示
                     echo "Error: Division by zero.";
                 }
                 break;
-                
-
             default:
                 // 未知の演算子に対するエラー処理
                 echo "正しい演算子を入力してください";
         }
-        
-}
+        return view('calculation', ['calc_type' => $calc_type, 'calc' => $calc]);
+    }
 }
--- resources/views/calculation.blade.php
+++ resources/views/calculation.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>割り算</title>
+    <title>{{ $calc_type }}算</title>
 </head>
 <body>
     <h1>計算アプリ</h1>
